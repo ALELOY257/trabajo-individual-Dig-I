1675ad922df978556bb087bb17bbd744e65f4806
ALELOY257 <lealalejandro422@gmail.com>
2026-05-22 09:17:39 -0500

    minor changes

--- calc/div/internal/comp_helper.v
+++ calc/div/internal/comp_helper.v
@@ -1,9 +1,9 @@
 module comp_helper #(parameter WIDTH=8)(
-    input [WIDTH-1:0] helper_ca2;
+    input [WIDTH-1:0] reg_ca2;
     output reg v;
 );
     always@(*)
-        if (helper_ca2<0)
+        if (reg_ca2<0)
             v <= 1;
         else 
             v <= 0;
