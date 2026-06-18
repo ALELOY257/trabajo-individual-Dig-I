0d49d3ae8fb2013fce52231d0601e151bb18ecdc
ALELOY257 <lealalejandro422@gmail.com>
2026-06-18 11:50:34 -0500

    add: ws2812_upload

---
+++ periphs/ws2812B/ws2812_upload.v
@@ -0,0 +1,14 @@
+module ws2812_upload(
+    input reset,
+    input clk,
+    output dout,
+) 
+    ws_array_top u_ws_array(
+        .clk(clk),
+        .reset(!reset),
+        .init_m(1),
+        .rst_cmd(0),
+        .dout(dout),
+        .done_m()
+    );
+endmodule
